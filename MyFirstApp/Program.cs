e6c6f8d26efc8954511a084b9e5486c9b262c24b
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-29 19:20:24 +0000

    Reorganize Program.cs to enable password generation logic

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,15 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-Console.WriteLine("What is your name?");
-//string? name = Console.ReadLine();
-var name = Console.ReadLine() ?? "Guest";
-var currentDate = DateTime.Now;
-Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:T}!");
-Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+// Console.WriteLine("What is your name?");
+// //string? name = Console.ReadLine();
+// var name = Console.ReadLine() ?? "Guest";
+// var currentDate = DateTime.Now;
+// Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:T}!");
+// Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
 
-// using PasswordGenerator;
+using PasswordGenerator;
 
-// var pwd = new Password();
-// var password = pwd.Next();
-// Console.WriteLine(password.ToString());
+var pwd = new Password();
+var password = pwd.Next();
+Console.WriteLine(password.ToString());
