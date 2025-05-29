272aca44868ce6fb6096dd08d907613527085e23
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-29 19:19:15 +0000

    Refactor Program.cs to prompt for user name and display current date

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("What is your name?");
+//string? name = Console.ReadLine();
+var name = Console.ReadLine() ?? "Guest";
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:T}!");
+Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+
+// using PasswordGenerator;
+
+// var pwd = new Password();
+// var password = pwd.Next();
+// Console.WriteLine(password.ToString());
