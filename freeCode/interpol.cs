82e180af2b5c3dd80cfc72deee08d03271d42a8e
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-07-03 22:00:07 +0000

    Add interpol.cs file with examples of string interpolation and temperature conversion

---
+++ freeCode/interpol.cs
@@ -0,0 +1,20 @@
+// Excerise: Use string interpolation to create a file path
+// string projectName = "ACME";
+// string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+// string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+// string russianLocation = $@"c:\Exercise\{projectName}\ru-Ru\data.txt";
+
+// Console.WriteLine($"View message in English:\n\t{englishLocation}");
+
+// Console.WriteLine($"\n{russianMessage}:\n\t{russianLocation}");
+
+// Console.WriteLine("Hello\rWorld!");
+// Console.WriteLine("Hello\nWorld!");
+// Console.WriteLine("Hello\r\nWorld!");
+
+// int result = 3 + 1 * 5 / 2;
+// Console.WriteLine($"Result of 3 + 1 * 5 / 2 = {result}");
+
+// int fahrenheit = 94;
+// decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+// Console.WriteLine("The temperature is " + celsius + " Celsius.");
