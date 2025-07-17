938ef6081691f5c77dd83b25c98dcebcfecb5168
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-07-17 21:10:12 +0000

    Add commented-out examples for string formatting and temperature conversion

---
+++ freeCode/temp.cs
@@ -0,0 +1,10 @@
+// Console.WriteLine("Hello\rWorld!");
+// Console.WriteLine("Hello\nWorld!");
+// Console.WriteLine("Hello\r\nWorld!");
+
+// int result = 3 + 1 * 5 / 2;
+// Console.WriteLine($"Result of 3 + 1 * 5 / 2 = {result}");
+
+// int fahrenheit = 94;
+// decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+// Console.WriteLine("The temperature is " + celsius + " Celsius.");
