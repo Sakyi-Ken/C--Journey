6623eb08dc0ad6b2b71e39d18d78f60e5ac6ca81
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-07-21 11:38:38 +0000

    Add commented-out subscription expiration logic with discount notifications

---
+++ freeCode/subscribe.cs
@@ -0,0 +1,45 @@
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+
+// if (daysUntilExpiration == 0)
+// {
+//   Console.WriteLine("Your subscription has expired.");
+// }
+// else if (daysUntilExpiration == 1)
+// {
+//   Console.WriteLine("Your subscription expires within a day!");
+//   discountPercentage = 20;
+// }
+// else if (daysUntilExpiration <= 5)
+// {
+//   Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+//   discountPercentage = 10;
+// }
+// else if (daysUntilExpiration <= 10)
+// {
+//   Console.WriteLine("Your subscription will expire soon. Renew now!");
+// }
+
+// if (discountPercentage > 0)
+// {
+//   Console.WriteLine($"Renew now and save {discountPercentage}%.");
+// }
+// else
+// {
+//   Console.WriteLine("No discounts available at this time.");
+// }
+// Console.WriteLine("Thank you for being a valued subscriber!");
+// Console.WriteLine("We appreciate your support.");
+// Console.WriteLine("Have a great day!");
+// Console.WriteLine("If you have any questions, please contact support.");
+// Console.WriteLine("Visit our website for more information.");
+// Console.WriteLine("Stay tuned for upcoming features and updates.");
+// Console.WriteLine("Your feedback is important to us.");
+// Console.WriteLine("Thank you for choosing our service.");
+// Console.WriteLine("We look forward to serving you again.");
+// Console.WriteLine("Don't forget to check your email for updates.");
+// Console.WriteLine("Follow us on social media for the latest news.");
+// Console.WriteLine("We value your privacy and security.");
+// Console.WriteLine("Your data is safe with us.");
+// Console.WriteLine("Thank you for your continued support."); 
